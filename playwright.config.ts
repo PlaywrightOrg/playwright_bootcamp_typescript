10ca4e2fd38e15b13c78f0fb6049a3457d35ba17
Luisina Helu Iseli <lu.helu7@gmail.com>
2024-09-24 13:47:16 -0300

    browser changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
+    /*{
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
@@ -60,7 +60,7 @@ export default defineConfig({
        name: 'Mobile Chrome',
        use: { ...devices['Pixel 5'] },
      },
-     {
+     /*{
        name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] },
     },
